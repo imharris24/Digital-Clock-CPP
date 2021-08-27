261a8b6dfa02f8034a0c6810dcdd8a16f3f67350
Muhammad Harris <78906230+imharris24@users.noreply.github.com>
2021-08-27 16:33:09 +0500

    Update 0.1.1
    - Fixed lag and distortion
    - Converted to Object Oriented Programming

--- src/Digital-Clock.cpp
+++ src/Digital-Clock.cpp
@@ -1,20 +1,27 @@
-//Digital Watch using CPP
+//Digital Clock using CPP
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream>
-#include <chrono>
-#include <ctime>    
-
+#include<iostream>
+#include<chrono>
+#include<ctime>
+#include<Windows.h>
 using namespace std;
+
+class Clock {
+public:
+	static void DisplayTime() {
+		auto CurrentTime = chrono::system_clock::now();
+		while (true) {
+			CurrentTime = chrono::system_clock::now();
+			time_t Current_T = chrono::system_clock::to_time_t(CurrentTime);
+			cout << "Date & Time : " << ctime(&Current_T) << "\n";
+			Sleep(1000);
+			system("cls");
+		}
+	}
+};
 
 int main()
 {
-	auto CurrentTime = chrono::system_clock::now();
-	while (true) {
-		CurrentTime = chrono::system_clock::now();
-		time_t Current_T = chrono::system_clock::to_time_t(CurrentTime);
-		cout << "Date & Time : " << ctime(&Current_T) << "\n";
-		system("cls");
-	}
+	Clock::DisplayTime();
 	return 0;
 }
